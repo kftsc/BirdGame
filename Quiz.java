60e94450b7844936c1f0eaced56aa234115ee699
Yixiong Wu <samwu@udel.edu>
2019-04-08 16:27:01 -0400

    add quiz class

---
+++ Quiz.java
@@ -0,0 +1,10 @@
+// author Yixiong Wu
+public class Quiz {
+	String question;
+	String answer;
+	
+	public Quiz (String question, String answer) {
+		this.answer = answer;
+		this.question = answer;
+	}
+}
