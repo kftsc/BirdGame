62a6cdd91a6c8a1bd482df5a280a523ba6080283
Yixiong Wu <samwu@udel.edu>
2019-04-08 14:38:10 -0400

    subtle changes

--- src/HitItem.java
+++ src/HitItem.java
@@ -1,5 +1,6 @@
-
+// author Sicheng Tian
 public class HitItem extends Element{
+	//represents which kind of item it is
 	ItemType it;
 	
 	public HitItem( int x, int y, ItemType i) {
@@ -7,7 +8,7 @@ public class HitItem extends Element{
 		it = i;
 	}
 	
-	// move the item, change x and y by adding incX and incY on x and y.
+	// move the item, change x and y by adding or subtracting incX and incY on x and y.
 	public void move(int incX, int incY) {
 		
 	}
