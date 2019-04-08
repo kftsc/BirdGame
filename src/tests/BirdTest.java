f053a0b0ff8c23889984225f3c8316b232bc4557
Yufan Wang <leeff@udel.edu>
2019-04-08 19:18:30 -0400

    junit test files created

---
+++ src/tests/BirdTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BirdTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
