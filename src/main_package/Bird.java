f2a8115cb689e87e79b7bce898b14590f90f0d0d
Rubai Bian <lnbyk@udel.edu>
2019-04-08 19:50:39 -0400

    test for bird finished

--- src/main_package/Bird.java
+++ src/main_package/Bird.java
@@ -13,6 +13,16 @@ public class Bird extends Element{
 		bt = b;
 	}
 	
+	//getter for x
+	public int getX() {	return this.x;}
+	// getter for y
+	public int getY() {	return this.y;}
+	
+	//getter for life;
+	public int getLife() {return this.life;};
+	
+	
+	
 	// move the bird, change x and y by adding incX and incY on x and y
 	public void move(int incX, int incY) {
 	
--- src/tests/BirdTest.java
+++ src/tests/BirdTest.java
@@ -4,12 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main_package.Bird;
+import main_package.BirdType;
+
 class BirdTest {
-		Bird osprey1 = new Bird(0,0,3,NH);
+		Bird osprey1 = new Bird(0,0,3,BirdType.NH);
 	@Test
 	void testMove() {
 		osprey1.move(1,1);
-		
+		assertEquals(1,osprey1.getX());
+		assertEquals(1,osprey1.getY());
+	}
+	
+	@Test
+	void testCollision() {
+		osprey1.collision();
+		assertEquals(2,osprey1.getLife());
+	}
+	
+	@Test
+	void testEat() {
+		osprey1.eat();
+		assertEquals(3,osprey1.getLife());
 	}
 
 }
